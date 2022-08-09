00524a9709b9174128ca1e3f2b698219d7643ea1
acothaha <acothaha76@gmail.com>
2022-08-09 16:01:23 +0700

    first push

--- day_1/case_1_group_by.sql
+++ day_1/case_1_group_by.sql
@@ -2,6 +2,7 @@ select id, first_name, last_name, school, max(salary) salary
 from teachers 
 group by school;
 
+
 select id, first_name, last_name, school, max(salary) salary
 from teachers 
 where school = 'Standford University'
